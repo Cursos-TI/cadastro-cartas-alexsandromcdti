491bf2e0dbd554f9a441a68a68e7a69519036157
alexsandromcdti <alexsandro.mcd.ti@gmail.com>
2025-12-06 20:16:54 -0300

    Add Carta 1 and Carta 2 data collection

---
+++ CartasSuper.Trunfo.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+int main() {
+
+    // ---------- CARTA 1 ----------
+    char estado1;
+    char codigo1[4];
+    char cidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos1;
+
+    printf("Cadastro da Carta 1:\n");
+
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado1);
+
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", codigo1);
+
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]s", cidade1);
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área (km²): ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontos1);
+
+
+    // ---------- CARTA 2 ----------
+    char estado2;
+    char codigo2[4];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos2;
+
+    printf("\nCadastro da Carta 2:\n");
+
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado2);
+
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", codigo2);
+
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]s", cidade2);
+
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Área (km²): ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontos2);
+
+
+    // ---------- EXIBIÇÃO ----------
+    printf("\n=============================\n");
